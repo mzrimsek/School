c42924a3e13e47778a5d0e433de4f22b83521ac1
Mike Zrimsek <mikezrimsek@gmail.com>
2015-02-03 19:41:35 -0800

    Update Dynamic arrays lab to dynamically grow array

--- CS41/Labs/Dynamic Arrays/src/Tester.java
+++ CS41/Labs/Dynamic Arrays/src/Tester.java
@@ -1,30 +1,42 @@
 /**
  * @author Mike Zrimsek
- * @version 01.21.2015
+ * @version 02.03.2015
  */
 
 import java.util.Scanner;
 
-public class Tester 
+public class Tester
 {
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Enter size of array: ");
-		int[] nums = new int[sc.nextInt()];
+		int size = sc.nextInt();
 		
-		for(int i = 0; i < nums.length; i++)
+		int count = 0;
+		int[] nums = new int[size];
+		while (count < size)
 		{
 			System.out.print("Enter number: ");
 			int n = sc.nextInt();
-			if(n == -1) break;
-			nums[i] = n;
+			if (n == -1) break;
+			nums[count] = n;
+			count++;
+			if (count == size) //going to add out of index
+			{
+				int[] temp = new int[size+1];
+				for (int i = 0; i < size; i++)
+					temp[i] = nums[i];
+				nums = temp; //resized array
+				size++;
+			}
 		}
 		
 		System.out.print("\n[ ");
-		for(int x : nums)
-			System.out.print(x + " ");
+		for (int i = 0; i < size; i++)
+			if(!(i == size-1 && nums[i] == 0))
+				System.out.print(nums[i] + " ");
 		System.out.println(" ]");
 		sc.close();
 	}
