a1804982bc7b44db115b2bc20d55a933893bf416
Mike Zrimsek <mikezrimsek@gmail.com>
2015-02-17 18:47:28 -0800

    Finished Array Based Queue lab

--- CS41/Labs/Array Based Queue/src/Tester.java
+++ CS41/Labs/Array Based Queue/src/Tester.java
@@ -1,7 +1,7 @@
 /**
  * 
  * @author Mike Zrimsek
- * @version 02.10.2015
+ * @version 02.17.2015
  *
  */
 
@@ -10,24 +10,97 @@ public class Tester
 	
 	public static void main(String[] args)
 	{
+		Queue q = new Queue();
+		System.out.println(q.dequeue());
+		q.enqueue(10);
+		q.enqueue(20);
+		q.enqueue(30);
+		q.enqueue(40);
+		q.enqueue(50);
+		q.enqueue(60);
+		q.enqueue(70);
+		q.enqueue(80);
+		q.enqueue(90);
+		q.enqueue(100);
+		System.out.println(q);
 		
+		System.out.println(q.dequeue());
+		System.out.println(q);
+		System.out.println(q.dequeue());
+		System.out.println(q);
+		System.out.println(q.dequeue());
+		System.out.println(q);
 		
+		q.enqueue(110);
+		q.enqueue(120);
+		q.enqueue(130);
+		q.enqueue(140);
+		
+		System.out.println(q);
+	}
+}
+
+class Queue
+{
+	final int MAX_SIZE = 10;
+	int count;
+	int[] nums;
+	int frontIndex;
+	int backIndex;
+	
+	public Queue()
+	{
+		nums = new int[MAX_SIZE];
+		count = 0;
+		frontIndex = -1;
+		backIndex = -1;
 	}
 	
-	class Queue
+	public void enqueue(int data)
 	{
-		final int MAX_SIZE = 10;
-		int count;
-		int[] nums;
-		int frontIndex;
-		int backIndex;
-		
-		public Queue()
+		if (count == MAX_SIZE)
 		{
-			nums = new int[MAX_SIZE];
-			count = 0;
-			frontIndex = -1;
-			backIndex = -1;
+			System.out.println("Queue at full capacity");
+			return;
 		}
+		nums[count] = data;
+		count++;
+	}
+	
+	public int dequeue()
+	{
+		if (empty())
+		{
+			System.out.println("Empty queue");
+			return Integer.MIN_VALUE;
+		}
+		else
+		{
+			int n = nums[10-count];
+			count--;
+			nums = shift(nums);
+			return n;
+		}
+	}
+	
+	public String toString()
+	{
+		String output = "[ ";
+		for (int x : nums)
+			output += x + ", ";
+		return output.substring(0, output.lastIndexOf(',')) + "]";
+	}
+	
+	public boolean empty()
+	{
+		return count == 0;
+	}
+	
+	private int[] shift(int[] n)
+	{
+		int[] temp = new int[n.length];
+		for(int i = 1; i < n.length; i++)
+			temp[i-1] = n[i];
+		return temp;
 	}
 }
