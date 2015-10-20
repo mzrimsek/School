999340675014cad1d53bc51e9fcb9462fefe30b9
Mike Zrimsek <mikezrimsek@gmail.com>
2015-10-20 16:29:31 -0400

    Added length counter for string and most logic for traversing bits.  Need to implement swap and cases for exiting loops

--- CS35101/mzrimsek/Project 1/selectionSort.asm
+++ CS35101/mzrimsek/Project 1/selectionSort.asm
@@ -5,7 +5,9 @@
 		.align 2		#align on next word boundary
 prompt:		.asciiz "Please enter 10 characters (upper/lowercase mixed):\n"
 		.align 2		#align on next word boundary
-display:	.asciiz "Sorted: "
+unsorted:	.asciiz "Unsorted: "
+		.align 2
+sorted:		.asciiz "Sorted: "
 		.align 2
 thestring:	.space 11		#hold user input
 		.text
@@ -20,19 +22,61 @@ main:
 		la $a0, thestring
 		li $a1, 11
 		syscall
-#do work on user input
-		la $t0, ($a0)		#store the string in $s0
-	Load:
-		lb $t1, ($t0)		#load character at address
-		addi $t0, $t0, 1	#increment address
-		lb $t1, ($t0)
-		bne $t1, $0, Load
-#display display string
-		la $a0, display		#display display prompt
+#display unsorted string
+		la $a0, unsorted	#display unsorted prompt
 		li $v0, 4
 		syscall
 #display thestring
 		la $a0, thestring	#display user input
+		li $v0, 4
+		syscall
+#do work on user input
+		la $s0, ($a0)		#store address of user input in $s0
+		add $t5, $0, $0		#length of input
+	CountLen:
+		lb $t1, ($s0)		#load character at address
+		addi $s0, $s0, 1	#increment address
+		addi $t5, $t5, 1	#increment length of string
+		bne $t1, $0, CountLen	#if not null character loop again
+		
+		la $s0, ($a0)		#reset address
+		
+		add $t0, $0, $0		#i = 0
+	ILoop:	
+		addi $t1, $t0, 1	#j = i + 1
+		add $t2, $t0, $0	#maxPos = i
+	JLoop:
+		add $t3, $s0, $t1	#get address of thestring[j]
+		lb $s1, ($t3)		#load thestring[j]
+		add $t3, $s0, $t2	#get address of thestring[maxPos]
+		lb $s2, ($t3)		#load thestring[maxPos]
+		sgt $t4, $s1, $s2	#check thestring[j] > thestring[maxPos]
+		beq $t4, $0, JElse	#if not greater than maxPos to else
+		add $t2, $t1, $0	#maxPos = j
+	JElse:
+		addi $t1, $t1, 1	#j++
+		slt $t4, $t1, $t5	#check j < string.length
+		bne $t4, $0, JLoop	#if not greater than string.length, loop again
+		
+	#if maxPos != i
+	#get address at thestring[i]
+	#load thestring[i]
+	#get address at thestring[maxPos]
+	#load thestring[maxPos]
+	#store value of thestring[maxPos] at address of thestring[i]
+	#store value of thestring[i] at address of thestring[maxPos]
+	#else
+	#i++
+	#if i = length break
+	#else
+	#do ILoop again
+		
+#display sorted string
+		la $a0, sorted		#display sorted prompt
+		li $v0, 4
+		syscall
+#display sorted thestring
+		la $a0, ($s0)		#display sorted user input
 		li $v0, 4
 		syscall
 #exit gracefully
