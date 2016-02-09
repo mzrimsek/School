40307eb1a2a1d898d5300611d18b9809c0502f2d
Mike Zrimsek <mikezrimsek@gmail.com>
2016-02-09 11:03:31 -0500

    Moved copyfile stuff to function

--- CS43203/mzrimsek/HW1/cp1.c
+++ CS43203/mzrimsek/HW1/cp1.c
@@ -9,39 +9,48 @@
 
 void oops(char *, char *);
 
-main(int ac, char *av[])
-{
-        int     in_fd, out_fd, n_chars;
-        char    buf[BUFFERSIZE];
-						/* check args 	*/
+void copyfile(char *, char *);
+
+main(int ac, char *av[]){
+        //check args
         if ( ac != 3 ){
                 fprintf( stderr, "usage: %s source destination\n", *av);
                 exit(1);
         }
-						/* open files	*/
-
-        if ( (in_fd=open(av[1], O_RDONLY)) == -1 )
-                oops("Cannot open ", av[1]);
-
-        if ( (out_fd=creat( av[2], COPYMODE)) == -1 )
-                oops( "Cannot create ", av[2]);
-
-						/* copy files	*/
-
-        while ( (n_chars = read(in_fd , buf, BUFFERSIZE)) > 0 )
-                if ( write( out_fd, buf, n_chars ) != n_chars )
-                        oops("Write error to ", av[2]);
-	if ( n_chars == -1 )
-			oops("Read error from ", av[1]);
-
-						/* close files	*/
-
-        if ( close(in_fd) == -1 || close(out_fd) == -1 )
-                oops("Error closing files","");
+        copyfile(av[1], av[2]);
 }
 
-void oops(char *s1, char *s2)
-{
+void copyfile(char *src_file, char *dest_file){
+        int     in_fd, out_fd, n_chars;
+        char    buf[BUFFERSIZE];
+        //open files
+        if ( (in_fd=open(src_file, O_RDONLY)) == -1 ){
+                oops("Cannot open ", src_file);
+        }
+
+
+        if ( (out_fd=creat( dest_file, COPYMODE)) == -1 ){
+                oops( "Cannot creat ", dest_file);
+        }
+
+        //copy files
+        while ( (n_chars = read(in_fd , buf, BUFFERSIZE)) > 0 ){
+                if ( write( out_fd, buf, n_chars ) != n_chars ){
+                        oops("Write error to ", dest_file);
+                }
+        }
+
+	if ( n_chars == -1 ){
+                oops("Read error from ", src_file);
+        }
+
+        //copy files
+        if ( close(in_fd) == -1 || close(out_fd) == -1 ){
+                oops("Error closing files","");
+        }
+}
+
+void oops(char *s1, char *s2){
         fprintf(stderr,"Error: %s ", s1);
         perror(s2);
         exit(1);
