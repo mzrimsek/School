23f0297d8379e71040418195fe66d2cf0ea78254
Mike Zrimsek <mikezrimsek@gmail.com>
2016-04-26 16:24:52 -0400

    pipe3 finished

--- CS43203/mzrimsek/HW5/pipe3.c
+++ CS43203/mzrimsek/HW5/pipe3.c
@@ -6,130 +6,64 @@ Homework 5 - pipe3
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#define oops(m, x) { perror(m); exit(x); }
+#define	oops(m)  { perror(m); exit(1); }
 
-main(int ac, char **av){
-    int first_pipe[2];
-    int second_pipe[2];
-    int pid;
+int pipeCommands(int ac, char *av[]){
+		int	thepipe[2];
+		pid_t	pid;
 
-    //check argument count
-    if(ac != 4){
-        fprintf(stderr, "usage: pipe3 cmd1 cmd2 cmd3\n");
-        exit(1);
-    }
+		//no commands to pipe
+		if (ac == 1){
+				fprintf(stderr,"usage: pipe3 cmd1 cmd2 ..\n");
+				exit(1);
+		}
 
-    //make first pipe
-    if(pipe(first_pipe) == -1){
-        oops("Cannot get pipe", 1);
-    }
+		//only one command
+		if (ac == 2){
+				execlp(av[1],av[1],NULL);
+				oops(av[1]);
+		}
 
-    //fork for first command
-    if((pid = fork()) == -1){
-        oops("Cannot fork", 2);
-    }
-    else{
-        dup2(first_pipe[1], 1);
+		//get a pipe
+		if (pipe(thepipe) == -1){
+				oops("Cannot get pipe");
+		}
 
-        close(first_pipe[0]);
-        close(first_pipe[1]);
+		//get a process
+		pid = fork();
+		if (pid == -1){
+				oops("Cannot fork");
+		}
 
-        execlp(av[1], av[1], NULL);
-        oops(av[1], 4);
-    }
+		//write stdout into pipe[1]
+		if (pid == 0){
+				if (dup2(thepipe[1], 1) == -1){
+						oops("dup2");
+				}
+				if (close(thepipe[0]) == -1 || close(thepipe[1]) == -1){
+						oops("close pipe");
+				}
+				pipeCommands(ac-1, av);
+		}
 
-    //make second pipe
-    if(pipe(second_pipe) == -1){
-        oops("Cannot get pipe", 1);
-    }
-
-    //fork for second command
-    if((pid = fork()) == -1){
-        oops("Cannot fork", 2);
-    }
-    else{
-        dup2(first_pipe[0], 0);
-        dup2(second_pipe[1], 1);
-
-        close(first_pipe[0]);
-        close(first_pipe[1]);
-        close(second_pipe[0]);
-        close(second_pipe[1]);
-
-        execlp(av[2], av[2], NULL);
-        oops(av[2], 4);
-    }
-
-    close(first_pipe[0]);
-    close(first_pipe[1]);
-
-    if((pid = fork()) != -1){
-        oops("Cannot fork", 2);
-    }
-    else{
-        dup2(second_pipe[0], 0);
-
-        close(second_pipe[0]);
-        close(second_pipe[1]);
-
-        execlp(av[3], av[3], NULL);
-        oops(av[3], 4);
-    }
+		//read stdin from pipe[0]
+		else {
+				if (dup2(thepipe[0], 0) == -1){
+						oops("dup2");
+				}
+				if (close(thepipe[0]) == -1 || close(thepipe[1]) == -1){
+						oops("close pipe");
+				}
+				execlp(av[ac-1], av[ac-1], NULL);
+				oops(av[ac-1]);
+		}
+		return 0;
 }
 
-// #define	oops(m,x)	{ perror(m); exit(x); }
-//
-// main(int ac, char **av)
-// {
-// 		int	thepipe[3],			/* two file descriptors	*/
-// 		newfd,				/* useful for pipes	*/
-// 		pid;				/* and the pid		*/
-//
-// 		//check num arguments
-// 		if (ac != 4){
-// 				fprintf(stderr, "usage: pipe3 cmd1 cmd2 cmd3\n");
-// 				exit(1);
-// 		}
-//
-// 		//get pipes
-// 		if (pipe(thepipe) == -1){
-// 				oops("Cannot get a pipe", 1);
-// 		}
-//
-// 	/* ------------------------------------------------------------ */
-// 	/*	now we have a pipe, now let's get two processes		*/
-//
-// 		if ((pid = fork()) == -1){		/* get a proc	*/
-// 				oops("Cannot fork", 2);
-// 		}
-//
-// 	/* ------------------------------------------------------------ */
-// 	/* 	Right Here, there are two processes			*/
-// 	/*             parent will read from pipe			*/
-//
-// 		if (pid > 0){			/* parent will exec av[2]	*/
-// 				close(thepipe[1]);	/* parent doesn't write to pipe	*/
-//
-// 				if (dup2(thepipe[0], 0) == -1){
-// 						oops("could not redirect stdin",3);
-// 				}
-//
-// 				close(thepipe[0]);	/* stdin is duped, close pipe	*/
-// 				execlp(av[2], av[2], NULL);
-// 				oops(av[2], 4);
-// 		}
-//
-// 	/*	 child execs av[1] and writes into pipe			*/
-//
-// 		close(thepipe[0]);		/* child doesn't read from pipe	*/
-//
-// 		if (dup2(thepipe[1], 1) == -1){
-// 				oops("could not redirect stdout", 4);
-// 		}
-//
-// 		close(thepipe[1]);		/* stdout is duped, close pipe	*/
-// 		execlp(av[1], av[1], NULL);
-// 		oops(av[1], 5);
-// }
+int main(int ac, char *av[])
+{
+		exit(pipeCommands(ac, av));
+}
