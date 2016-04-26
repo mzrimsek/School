828a754a86295d4198a226763ae863fc0e2f8ed2
Mike Zrimsek <mikezrimsek@gmail.com>
2016-04-26 12:55:29 -0400

    Update

--- CS43203/mzrimsek/HW5/pipe3.c
+++ CS43203/mzrimsek/HW5/pipe3.c
@@ -6,22 +6,22 @@
 
 main(int ac, char **av)
 {
-		int	thepipe[3],			/* two file descriptors	*/
+		int	thepipe[2],			/* two file descriptors	*/
 		newfd,				/* useful for pipes	*/
 		pid;				/* and the pid		*/
 
-		if ( ac != 4 ){
-				fprintf(stderr, "usage: pipe3 cmd1 cmd2 cmd3\n");
+		if (ac != 3){
+				fprintf(stderr, "usage: pipe3 cmd1 cmd2\n");
 				exit(1);
 		}
-		if ( pipe( thepipe ) == -1 ){		/* get a pipe		*/
+		if (pipe( thepipe ) == -1){		/* get a pipe		*/
 				oops("Cannot get a pipe", 1);
 		}
 
 	/* ------------------------------------------------------------ */
 	/*	now we have a pipe, now let's get two processes		*/
 
-		if ( (pid = fork()) == -1 ){		/* get a proc	*/
+		if ((pid = fork()) == -1){		/* get a proc	*/
 				oops("Cannot fork", 2);
 		}
 
@@ -29,15 +29,15 @@ main(int ac, char **av)
 	/* 	Right Here, there are two processes			*/
 	/*             parent will read from pipe			*/
 
-		if ( pid > 0 ){			/* parent will exec av[2]	*/
+		if (pid > 0){			/* parent will exec av[2]	*/
 				close(thepipe[1]);	/* parent doesn't write to pipe	*/
 
-				if ( dup2(thepipe[0], 0) == -1 ){
+				if (dup2(thepipe[0], 0) == -1){
 						oops("could not redirect stdin",3);
 				}
 
 				close(thepipe[0]);	/* stdin is duped, close pipe	*/
-				execlp( av[2], av[2], NULL);
+				execlp(av[2], av[2], NULL);
 				oops(av[2], 4);
 		}
 
@@ -45,11 +45,11 @@ main(int ac, char **av)
 
 		close(thepipe[0]);		/* child doesn't read from pipe	*/
 
-		if ( dup2(thepipe[1], 1) == -1 ){
+		if (dup2(thepipe[1], 1) == -1){
 				oops("could not redirect stdout", 4);
 		}
 
 		close(thepipe[1]);		/* stdout is duped, close pipe	*/
-		execlp( av[1], av[1], NULL);
+		execlp(av[1], av[1], NULL);
 		oops(av[1], 5);
 }
