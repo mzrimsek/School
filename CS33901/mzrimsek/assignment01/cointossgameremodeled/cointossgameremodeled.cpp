aa9c833772bc060403307502e97457b7a6556f85
Mike Zrimsek <mikezrimsek@gmail.com>
2016-09-19 21:21:23 -0400

    working on refactoring code

---
+++ CS33901/mzrimsek/assignment01/cointossgameremodeled/cointossgameremodeled.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void handleCorrectGuess(int* streak, int playsLeft)
+{
+		*streak = *streak + 1;
+		cout << "\n\nYou guessed correct! "<< playsLeft << " plays left, with a streak of " << *streak << "!";
+}
+
+int main()
+{
+		int guess;
+		int playsLeft = 5;
+		int streak = 0;
+		char playAgain = 'y';
+		char cashIn = 'n';
+
+    cout <<"\n\n			Welcome to Coin Toss Game!!" <<endl;
+    cout <<"\n\nRules:\nGuess 5 coin tosses in a row to win the 1,000,000 dollar Jackpot.";
+    cout <<"\nGuess 3 in a row, and cash in for 100 dollars";
+
+		while (playAgain =='y')
+		{
+				do
+				{
+						srand(static_cast<unsigned int>(time(0)));
+						int coinFace = rand() % 2+1;
+
+						cout << "\n\nHeads or Tails? (1 or 2)" <<endl;
+						cin >> guess;
+						--playsLeft;
+
+            if(guess == coinFace & coinFace == 1)
+            {
+            		cout << "\nThe coin landed Heads up!";
+		            handleCorrectGuess(&streak, playsLeft);
+            }
+            else if(guess == coinFace & coinFace == 2)
+            {
+            		cout << "\nThe coin landed Tails up! ";
+		            handleCorrectGuess(&streak, playsLeft);
+            }
+            else if(guess != coinFace & coinFace == 1)
+            {
+            		cout << "\nThe coin landed Heads up! ";
+		            streak = 0;
+            		cout << "\n\nUnlucky, you were wrong! " << playsLeft << " plays left!";
+            }
+            else if(guess != coinFace & coinFace == 2)
+            {
+								cout << "\nThe coin landed Tails up! ";
+            		streak = 0;
+            		cout << "\n\nUnlucky, you were wrong! "<< playsLeft<< " plays left!";
+            }
+
+						if(streak == 3)
+						{
+								cout << "\n\nYou have reached a streak of three!! Cash in?(y/n)";
+								cin >>cashIn;
+						}
+						else if(streak == 5)
+						{
+                cashIn = 'y';
+						}
+
+						if(playsLeft == 0)
+						{
+								cashIn = 'y';
+						}
+				}while (cashIn != 'y');
+
+        if(streak == 3 && cashIn == 'y')
+				{
+						cout << "\n\nCONGRATULATIONS!!";
+						cout << "\nYou have cashed in for 100 dollars with a streak of 3!"<<endl;
+				}
+				else if(streak == 5 && cashIn == 'y')
+				{
+						cout << "\n\nCONGRATULATIONS!!";
+        		cout << "\nYou win the Jackpot of 1,000,000 dollars with a streak of FIVE!!!"<<endl;
+				}
+				else if(streak != 5 && cashIn == 'y')
+				{
+						cout << "\n\nYou have run out of plays! Better luck next time!"<<endl;
+				}
+
+				cout << "\n\nDo you wish to play again?(y/n)" ;
+				cin >> playAgain;
+
+				playsLeft = 5;
+				cashIn = 'n';
+				streak = 0;
+		}
+
+		cout << "\n\nOk, cya!!"<<endl;
+    return 0;
+}
