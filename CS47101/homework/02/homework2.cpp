7bff70550a1c8b96a1e473ee99707dab9a580810
Mike Zrimsek <mikezrimsek@gmail.com>
2016-10-10 20:15:34 -0400

    removed unnecessary stuff

--- CS47101/homework/02/homework2.cpp
+++ CS47101/homework/02/homework2.cpp
@@ -7,16 +7,6 @@
 static int WINDOW_WIDTH = 500;
 static int WINDOW_HEIGHT = 500;
 
-void init()
-{
-  glClearColor(0.0, 0.0, 0.0, 1.0);
-  glColor3f(1.0, 1.0, 1.0);
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-}
-
 void myDisplay()
 {
   glClear(GL_COLOR_BUFFER_BIT);
@@ -33,8 +23,6 @@ int main(int argc, char** argv)
   glutCreateWindow("Homework 2");
 
   glutDisplayFunc(myDisplay);
-
-  init();
 
   glutMainLoop();
 }
