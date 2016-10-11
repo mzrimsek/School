b6f92ae37dd7b932b419a9e8d81d0149dfd85e1a
Mike Zrimsek <mikezrimsek@gmail.com>
2016-10-11 01:38:24 -0400

    wip

--- CS47101/homework/02/homework2.cpp
+++ CS47101/homework/02/homework2.cpp
@@ -2,17 +2,23 @@
 // Computer Graphics
 // Homework 2
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 #include <GL/glut.h>
 
 static int WINDOW_WIDTH = 500;
 static int WINDOW_HEIGHT = 500;
+static double DEGREE_TO_RADIAN = 3.14159/180;
 
-static int mainMenuId;
-static int mainMenuSelection = 0;
+int mainMenuId;
+int mainMenuSelection = 0;
 
-static int rectangleIdle = 0;
-static int circleIdle = 0;
+static int RECTANGLES_MENU_SELECTION = 1;
+static int CIRCLE_MENU_SELECTION = 2;
+static int RUBBERBANDING_CIRCLE_MENU_SELECTION = 3;
+
+double circle_x = -0.6, circle_y = -0.6;
 
 void mainMenu(int value)
 {
@@ -23,13 +29,23 @@ void mainMenu(int value)
 void createMainMenu()
 {
   mainMenuId = glutCreateMenu(mainMenu);
-  glutAddMenuEntry("Rectangles", 1);
-  glutAddMenuEntry("Circle", 2);
-  glutAddMenuEntry("Rubberbanding Circle", 3);
+  glutAddMenuEntry("Rectangles", RECTANGLES_MENU_SELECTION);
+  glutAddMenuEntry("Circle", CIRCLE_MENU_SELECTION);
+  glutAddMenuEntry("Rubberbanding Circle", RUBBERBANDING_CIRCLE_MENU_SELECTION);
   glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
-void keyboard(unsigned char key, int x, int y)
+void mouseCallback(int btn, int state, int x, int y)
+{
+
+}
+
+void mouseMotionCallback(int x, int y)
+{
+
+}
+
+void keyboardCallback(unsigned char key, int x, int y)
 {
   switch(key)
   {
@@ -39,41 +55,59 @@ void keyboard(unsigned char key, int x, int y)
   glutPostRedisplay();
 }
 
-void display()
+void idleCallback()
+{
+  if(mainMenuSelection == CIRCLE_MENU_SELECTION)
+  {
+    double circleStep = .0001;
+    circle_x += circleStep;
+    circle_y += circleStep;
+  }
+  
+  glutPostRedisplay();
+}
+
+void drawCircle(double x, double y, double radius)
+{
+  glBegin(GL_LINE_LOOP);
+    for(double i = 0; i < 360; i++)
+    {
+      double degreeInRadian = i*DEGREE_TO_RADIAN;
+      glVertex2f(x+cos(degreeInRadian)*radius, y+sin(degreeInRadian)*radius);
+    }
+  glEnd();
+}
+
+void displayCallback()
 {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  if(mainMenuSelection == 1)
+  if(mainMenuSelection == RECTANGLES_MENU_SELECTION)
   {
+    //red rectangle moves with mouse when left click is down
     glBegin(GL_POLYGON);
       glColor3f(1.0, 0.0, 0.0);
-      glVertex2f(-0.25, -0.25);
+      glVertex2f(0.33, 0.75);
+      glVertex2f(0.33, 0.25);
+      glVertex2f(0.66, 0.25);
+      glVertex2f(0.66, 0.75);
+    glEnd();
 
-      glColor3f(1.0, 0.0, 0.0);
-      glVertex2f(-0.25, 0.25);
-
-      glColor3f(1.0, 0.0, 0.0);
-      glVertex2f(0.5, 0.25);
-
-      glColor3f(1.0, 0.0, 0.0);
-      glVertex2f(0.5, -0.25);
+    //yellow rectangle moves with mouse
+    glBegin(GL_POLYGON);
+      glColor3f(1.0, 1.0, 0.0);
+      glVertex2f(-0.75, -0.75);
+      glVertex2f(-0.75, -0.25);
+      glVertex2f(0.25, -0.25);
+      glVertex2f(0.25, -0.75);
     glEnd();
   }
-  else if(mainMenuSelection == 2)
+  else if(mainMenuSelection == CIRCLE_MENU_SELECTION)
   {
-    double radius = .25;
-    double pi = 3.1415926535897932384626433832795;
-    glBegin(GL_LINE_LOOP);
-		  for(double i = 0; i < 2 * pi; i += pi / 24)
-      {
-        glColor3f(0.0, 0.0, 1.0);
-        double x = cos(i) * radius;
-        double y = sin(i) * radius;
-        glVertex3f(x-(1-radius), y-(1-radius), 0.0);
-      }
-		glEnd();
+    glColor3f(0.0, 0.0, 1.0);
+    drawCircle(circle_x, circle_y, 0.25);
   }
-  else if(mainMenuSelection == 3)
+  else if(mainMenuSelection == RUBBERBANDING_CIRCLE_MENU_SELECTION)
   {
     
   }
@@ -90,10 +124,13 @@ int main(int argc, char** argv)
   glutCreateWindow("Homework 2");
 
   createMainMenu();
-  glutKeyboardFunc(keyboard);
+  glutMouseFunc(mouseCallback);
+  glutMotionFunc(mouseMotionCallback);
+  glutKeyboardFunc(keyboardCallback);
+  glutIdleFunc(idleCallback);
 
   glClearColor(0.0, 0.0, 0.0, 0.0);
-  glutDisplayFunc(display);
+  glutDisplayFunc(displayCallback);
 
   glutMainLoop();
   return 0;
