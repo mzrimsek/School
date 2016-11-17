2e71a481d8640c0b20f7027d32263b4e0163fb93
Mike Zrimsek <mikezrimsek@gmail.com>
2016-11-16 19:50:02 -0500

    rendering 3 balls with separate light sources

--- CS47101/homework/03/homework3.cpp
+++ CS47101/homework/03/homework3.cpp
@@ -9,9 +9,21 @@ GLfloat mat_specular[] = { 1.0, 1.0, 0.0, 1.0 };
 GLfloat mat_shininess[] = { 100.0 };
 GLfloat mat_emission[] = { 1.0, 1.0, 0.1, 1.0 };
 GLfloat default_emission[] = { 0.0, 0.0, 0.0, 1.0 };
-GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+
+//red ball
 GLfloat red[] = { 1.0, 0.0, 0.0, 1.0 };
-GLfloat light[] = { 1.0, 1.0, 1.0 };
+GLfloat red_light[] = { 1.0, 1.0, 1.0 };
+GLfloat red_light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+
+//blue ball
+GLfloat blue[] = { 0.0, 0.0, 1.0, 1.0 };
+GLfloat blue_light[] = { 1.0, 1.0, 1.0 };
+GLfloat blue_light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+
+//green ball
+GLfloat green[] = { 0.0, 1.0, 0.0, 1.0 };
+GLfloat green_light[] = { 1.0, 1.0, 1.0 };
+GLfloat green_light_position[] = { 1.0, 1.0, 1.0, 0.0 };
 
 void init(void)
 {
@@ -20,25 +32,59 @@ void init(void)
 	glShadeModel(GL_SMOOTH);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    //red light
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, red_light);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, red_light);
+	glLightfv(GL_LIGHT0, GL_POSITION, red_light_position);
+
+    //blue light
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, blue_light);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, blue_light);
+	glLightfv(GL_LIGHT1, GL_POSITION, blue_light_position);
+
+    //green light
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, green_light);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, green_light);
+	glLightfv(GL_LIGHT2, GL_POSITION, green_light_position);
+
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_DEPTH_TEST);
 }
+
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
+
+    //red ball
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, red);
 	glEnable(GL_LIGHT0);
-	glTranslatef(0.0, 0.0, 0.0);
+	glTranslatef(0.75, -0.5, 0.0);
 	glutSolidSphere(0.4, 20, 16);
 	glPopMatrix();
 	glDisable(GL_LIGHT0);
+
+    //blue ball
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, blue);
+	glEnable(GL_LIGHT1);
+	glTranslatef(0.0, 0.5, 0.0);
+	glutSolidSphere(0.4, 20, 16);
+	glPopMatrix();
+	glDisable(GL_LIGHT1);
+
+    //green ball
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, green);
+	glEnable(GL_LIGHT2);
+	glTranslatef(-0.75, -1.0, 0.0);
+	glutSolidSphere(0.4, 20, 16);
+	glPopMatrix();
+	glDisable(GL_LIGHT2);
+
 	glFlush();
 }
+
 void reshape(int w, int h)
 {
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
