c8cf5caa0cf1846a39b1f810c54cd1c400a95f13
Mike Zrimsek <mikezrimsek@gmail.com>
2016-12-06 16:19:15 -0500

    formatting

--- CS47101/homework/bonus/bonus.cpp
+++ CS47101/homework/bonus/bonus.cpp
@@ -67,7 +67,7 @@ void renderLastName()
 
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
     glColor3f (1.0, 1.0, 1.0);
 
     glRasterPos2i(15, 55);
@@ -90,17 +90,17 @@ void reshape(int width, int height)
 
 int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glMatrixMode(GL_PROJECTION);
-	glutInitWindowSize(100, 100);
+    glutInitWindowSize(100, 100);
     glutInitWindowPosition(100, 100);
-	glutCreateWindow("Name");
+    glutCreateWindow("Name");
 
     init();
 
     glutReshapeFunc(reshape);
-	glutDisplayFunc(display);
-	glutMainLoop();
-	return 0;
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
