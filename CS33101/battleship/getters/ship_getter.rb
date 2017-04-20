28ed929594783bce2ff1c69da5a2a48c5c16caa2
Mike Zrimsek <mikezrimsek@gmail.com>
2017-04-19 20:34:49 -0400

    add getter to get all relevant ship info

---
+++ CS33101/battleship/getters/ship_getter.rb
@@ -0,0 +1,40 @@
+require_relative '../models/location'
+require_relative '../models/ship'
+
+class ShipGetter
+    private_class_method def self.get_random_location
+        rand_num_coord = rand(0...6)
+        rand_char_coord = rand(65...70).chr
+
+        Location.new(rand_num_coord, rand_char_coord)
+    end
+
+    private_class_method def self.get_random_locations(num_locations)
+        locations = []
+
+        i = 0
+        while i < num_locations do
+            new_loc = get_random_location
+            if !locations.include?(new_loc)
+                locations.push(new_loc)
+                i += 1
+            end
+        end
+
+        locations
+    end
+    
+    def self.get_ships(num_ships)
+        ships = []
+
+        locations = get_random_locations(num_ships)
+        locations.each do |loc|
+            newShip = Ship.new(loc, false)
+            ships.push(newShip)
+        end
+        
+        ships
+    end
+end
+
+puts ShipGetter.get_ships(5)
