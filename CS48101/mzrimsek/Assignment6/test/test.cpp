b5bcf8c44eed994b902e52051def0503254bed63
Mike Zrimsek <mikezrimsek@gmail.com>
2017-04-29 14:43:21 -0500

    successfully running a python program and reading it into the c++ program

--- CS48101/mzrimsek/Assignment6/test/test.cpp
+++ CS48101/mzrimsek/Assignment6/test/test.cpp
@@ -5,11 +5,23 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *fp = popen("python test.py 1 2", "rt");
+    FILE *fp;
     char buf[255];
+    char *COMMAND = "python test.py 6 7";
+
+    #ifdef _WIN32
+        fp = _popen(COMMAND, "rt");
+    #else
+        fp = popen(COMMAND, "rt");
+    #endif
 
     fgets(buf, 255, fp);
-    fclose(fp);
+
+    #ifdef _WIN32
+        _pclose(fp);
+    #else
+        pclose(fp);
+    #endif
 
     printf("%s", buf);
     
