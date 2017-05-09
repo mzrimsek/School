385caf96bccb0b85155fd1ba5605122d3b157785
Mike Zrimsek <mikezrimsek@gmail.com>
2017-05-08 22:02:42 -0500

    wip

--- CS48101/mzrimsek/Assignment6/run_neural_net.py
+++ CS48101/mzrimsek/Assignment6/run_neural_net.py
@@ -1,5 +1,7 @@
 import os
+import sys
 
 neural_src = os.getenv('NEURAL_SRC')
+sys.path.append(neural_src);
 os.chdir(neural_src)
 import mnist_loader
---
+++ CS48101/mzrimsek/FinalIteration/final/Final.h
@@ -0,0 +1,82 @@
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+#include "OgreManualObject.h"
+#include "btBulletDynamicsCommon.h"
+#include "btHeightfieldTerrainShape.h"
+#include "BaseApplication.h"
+#include <cstdlib>
+#include <random>
+#include <CEGUI/CEGUI.h>
+#include <CEGUI/RendererModules/Ogre/Renderer.h>
+#include <vector>
+#include <string>
+#include "collisionStructs.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+ 
+class TutorialApplication : public BaseApplication
+{
+public:
+  TutorialApplication();
+  virtual ~TutorialApplication();
+ 
+protected:
+  virtual void createScene();
+  void createNinja();
+  void assignItems(Ogre::SceneNode * node, Ogre::Entity * entity);
+  void createOgre(std::string name, btScalar mass, btVector3 & Position);
+  void createOgres(int numOgres);
+  virtual void createFrameListener();
+  virtual void destroyScene();
+  bool isNinja(std::string name);
+  bool isOgre(std::string name);
+  virtual bool frameRenderingQueued(const Ogre::FrameEvent& fe);
+  virtual void createCamera();
+  virtual void createViewports();
+  virtual void handleAnimations(const Ogre::FrameEvent& evt);
+  bool frameStarted(const Ogre::FrameEvent &evt);
+  bool frameEnded(const Ogre::FrameEvent & evt);
+  void RemoveObject(ogreObject * object, int index);
+ 
+private:
+  CEGUI::OgreRenderer* mRenderer;
+  void defineTerrain(long x, long y);
+  void initBlendMaps(Ogre::Terrain* terrain);
+  void configureTerrainDefaults(Ogre::Light* light);
+  bool processUnbufferedInput(const Ogre::FrameEvent& fe);
+  void createBulletSim(void);  
+  void CheckCollisions();
+  ogreObject* getOgreObject(const btCollisionObject * obj);
+ 
+  bool mTerrainsImported;
+  Ogre::TerrainGroup* mTerrainGroup;
+  Ogre::TerrainGlobalOptions* mTerrainGlobals;
+ 
+  OgreBites::Label* mInfoLabel;
+ 
+  Ogre::AnimationState*		mAnimationState;
+  Ogre::Entity*				mEntity;
+
+  btDefaultCollisionConfiguration* collisionConfiguration;
+  btCollisionDispatcher* dispatcher;
+  btBroadphaseInterface* overlappingPairCache;
+  btSequentialImpulseConstraintSolver* solver;
+  btDiscreteDynamicsWorld* dynamicsWorld;
+  btCollisionShape* groundShape;
+  btAlignedObjectArray<btCollisionShape*> collisionShapes;
+
+  ogreObject* ptrToOgreObject;
+  ogreObject* ptrToNinja;
+  std::vector<ogreObject *> ptrToOgreObjects;
+
+  CEGUI::Window *pointsLabel;
+  int forwardFlag;
+  int points;
+};
+ 
+
